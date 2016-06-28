41a18dda604412cb488e192a9aac4349e06394c7
Max Wittal <max.wittal@mwittal.de>
2016-06-28 04:56:19 -0700

    added Object

---
+++ include/vnl/Object.h
@@ -0,0 +1,25 @@
+/*
+ * Object.h
+ *
+ *  Created on: Jun 28, 2016
+ *      Author: mad
+ */
+
+#ifndef INCLUDE_VNL_OBJECT_H_
+#define INCLUDE_VNL_OBJECT_H_
+
+
+
+namespace vnl {
+
+class Object {
+public:
+	
+	
+	
+};
+
+
+}
+
+#endif /* INCLUDE_VNL_OBJECT_H_ */
